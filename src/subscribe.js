b355c9a072e0a42c9f0b2e2c673ba7ea80c2f561
Tim Hall <tim.hall.engr@gmail.com>
2018-07-15 13:27:41 -0400

    Add tests and fix subscribe issues

--- src/subscribe.js
+++ src/subscribe.js
@@ -1,7 +1,7 @@
 import { SVELTE_OBSERVABLE } from './utils';
 import unsubscribe from './unsubscribe';
 
-export default function subscribe({ changed, current }) {
+export default function subscribe({ changed, current, previous }) {
   // Load subscriptions from component
   // (if none found, assume first run and attach destroy listener to unsubscribe)
   let subscriptions = this[SVELTE_OBSERVABLE];
@@ -13,15 +13,21 @@ export default function subscribe({ changed, current }) {
   // Check for observables and subscribe / unsubscribe as-needed
   for (const key in changed) {
     const value = current[key];
-    const is_observable = value && value[SVELTE_OBSERVABLE];
-    const previous = subscriptions[key];
+    const observable = value && value[SVELTE_OBSERVABLE];
+    const previous_observable =
+      previous && previous[key] && previous[key][SVELTE_OBSERVABLE];
 
-    if (previous && (is_observable || previous.value !== value)) {
-      // Observable was overwritten, unsubscribe
-      previous.unsubscribe();
+    if (observable && observable === previous_observable) {
+      continue;
     }
 
-    if (is_observable) {
+    const existing = subscriptions[key];
+    if (existing) {
+      // Observable was overwritten, unsubscribe
+      existing.unsubscribe();
+    }
+
+    if (observable) {
       subscriptions[key] = createSubscription(this, key, value);
     }
   }
@@ -32,8 +38,7 @@ function createSubscription(component, key, observer) {
   // - First change, resolve initial deferred value
   // - Subsequent changes, set on component
   const observable = observer[SVELTE_OBSERVABLE];
-  const subscription = { value: observer, unsubscribe: null };
-  let initial = false;
+  let initial = true;
 
   const replace = value => {
     if (initial) {
@@ -42,17 +47,14 @@ function createSubscription(component, key, observer) {
     } else {
       // Continue exposing underlying observable on set values
       value[SVELTE_OBSERVABLE] = observable;
-      subscription.value = value;
-
       component.set({ [key]: value });
     }
   };
 
-  const { unsubscribe } = observable.subscribe({
+  const subscription = observable.subscribe({
     next: value => replace(Promise.resolve(value)),
     error: error => replace(Promise.reject(error))
   });
-  subscription.unsubscribe = unsubscribe;
 
   return subscription;
 }
